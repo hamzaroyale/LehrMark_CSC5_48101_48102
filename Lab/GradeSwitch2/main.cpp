5fcf4683afd0e731e3523052adf91724f926c841
ml1150258 <mark.lehr@rcc.edu>
2016-10-03 10:24:18 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Lab/GradeSwitch2/main.cpp
+++ Lab/GradeSwitch2/main.cpp
@@ -27,8 +27,15 @@ int main(int argc, char** argv) {
     
     //Process values -> Map inputs to Outputs
     switch(score){
-        case 100:case 99:case 98: case 97:case 96:
-        case 95:case 94:case 93: case 92:case 91:
+        case 100:case 99:
+        case 98: 
+        case 97:
+        case 96:
+        case 95:
+        case 94:
+        case 93: 
+        case 92:
+        case 91:
         case 90:grade='A';break;
         case 89:case 88: case 87:case 86:
         case 85:case 84:case 83: case 82:case 81:
