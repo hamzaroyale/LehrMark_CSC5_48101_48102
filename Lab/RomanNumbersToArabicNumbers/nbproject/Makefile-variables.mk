e1287fb44ecd25bcf4eb53a7a720705654231157
ml1150258 <mark.lehr@rcc.edu>
2016-10-17 10:07:00 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

---
+++ Lab/RomanNumbersToArabicNumbers/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=romannumberstoarabicnumbers
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/romannumberstoarabicnumbers
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=romannumberstoarabicnumbers.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/romannumberstoarabicnumbers.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=romannumberstoarabicnumbers
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/romannumberstoarabicnumbers
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=romannumberstoarabicnumbers.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/romannumberstoarabicnumbers.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
