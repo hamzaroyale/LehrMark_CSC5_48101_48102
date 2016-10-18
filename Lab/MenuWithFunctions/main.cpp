ccd912c9122c43cc6afeffe7fb734542c3d71e82
ml1150258 <mark.lehr@rcc.edu>
2016-10-18 10:09:46 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Lab/MenuWithFunctions/main.cpp
+++ Lab/MenuWithFunctions/main.cpp
@@ -1,8 +1,8 @@
 /* 
    File:   main
    Author: Dr. Mark E. Lehr
-   Created on October 10th, 9:30 AM
-   Purpose:  Menu without Functions
+   Created on October 17th, 8:30 AM
+   Purpose:  Menu with Functions
  */
 
 //System Libraries
@@ -14,6 +14,16 @@ using namespace std;  //Name-space used in the System Library
 //Global Constants
 
 //Function prototypes
+void problem1();
+void problem2();
+void problem3();
+void problem4();
+void problem5();
+void problem6();
+void problem7();
+void problem8();
+void problem9();
+void problem10();
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
@@ -38,79 +48,96 @@ int main(int argc, char** argv) {
 
         //Go to the Problem
         switch(menuItm){
-            case 1:{
-                cout<<"Inside Problem A"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }
-            case 2:{
-                cout<<"Inside Problem B"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }
-            case 3:{
-                cout<<"Inside Problem C"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }
-            case 4:{
-                cout<<"Inside Problem D"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }
-            case 5:{
-                cout<<"Inside Problem E"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }
-            case 6:{
-                cout<<"Inside Problem F"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }
-            case 7:{
-                cout<<"Inside Problem G"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }
-            case 8:{
-                cout<<"Inside Problem H"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }
-            case 9:{
-                cout<<"Inside Problem I"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }
-            case 10:{
-                cout<<"Inside Problem J"<<endl;
-                //Input values
-                //Process values -> Map inputs to Outputs 
-                //Display Output
-                break;
-            }//End Case
+            case 1:problem1();break;
+            case 2:problem2();break;
+            case 3:problem3();break;
+            case 4:problem4();break;
+            case 5:problem5();break;
+            case 6:problem6();break;
+            case 7:problem7();break;
+            case 8:problem8();break;
+            case 9:problem9();break;
+            case 10:problem10();
         }//End the Switch/Case
     }while(menuItm>0&&menuItm<=10);//Ends the Do-While Loop
 
     //Exit Program
     return 0;
+}
+
+//000000011111111112222222222333333333344444444445555555555666666666677777777778
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+//*************************   Problem 1 ****************************************
+//Purpose:  Put function purpose here!
+//Inputs:   Inputs to the function here  -> Description, Range, Units
+//Output:   Outputs to the function here -> Description, Range, Units
+//******************************************************************************
+void problem1(){
+    cout<<"Inside Problem A"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
+}
+
+void problem2(){
+    cout<<"Inside Problem B"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
+}
+
+void problem3(){
+    cout<<"Inside Problem C"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
+}
+
+void problem4(){
+    cout<<"Inside Problem D"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
+}
+
+void problem5(){
+    cout<<"Inside Problem E"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
+}
+
+void problem6(){
+    cout<<"Inside Problem F"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
+}
+
+void problem7(){
+    cout<<"Inside Problem G"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
+}
+
+void problem8(){
+    cout<<"Inside Problem H"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
+}
+
+void problem9(){
+    cout<<"Inside Problem I"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
+}
+
+void problem10(){
+    cout<<"Inside Problem J"<<endl;
+    //Input values
+    //Process values -> Map inputs to Outputs 
+    //Display Output
 }
