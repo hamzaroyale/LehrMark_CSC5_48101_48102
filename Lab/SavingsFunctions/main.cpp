2b967b620eee0350f1176f5008c7bac3d9caf510
ml1150258 <mark.lehr@rcc.edu>
2016-10-20 12:33:34 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Lab/SavingsFunctions/main.cpp
+++ Lab/SavingsFunctions/main.cpp
@@ -21,6 +21,7 @@ float save1(float,float,int);
 float save2(float,float,int);
 float save3(float,float,int);
 float save4(float,float,int);
+float save5(float,float,int);
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
@@ -30,9 +31,9 @@ int main(int argc, char** argv) {
     
     //Input values
     pv=100.0f;               //$100
-    intRate=8/PERCENT;       //8%
-    //nComp=9;               //9 years
-    nComp=72/intRate/PERCENT;//By the rule of 72
+    intRate=9/PERCENT;       //8%
+    nComp=90;               //9 years
+    //nComp=72/intRate/PERCENT+0.5;//By the rule of 72
     
     //Process values -> Map inputs to Outputs then Display Output
     cout<<fixed<<setprecision(2)<<showpoint;
@@ -43,10 +44,34 @@ int main(int argc, char** argv) {
     cout<<"Future  Value  (Exp,Log)    =$"<<save2(pv,intRate,nComp)<<endl;
     cout<<"Future  Value  (For-Loop)   =$"<<save3(pv,intRate,nComp)<<endl;
     cout<<"Future  Value  (Recursion)  =$"<<save4(pv,intRate,nComp)<<endl;
+    cout<<"Future  Value  (Banking)    =$"<<save5(pv,intRate,nComp)<<endl;
 
     //Exit Program
     return 0;
 }
+
+//000000011111111112222222222333333333344444444445555555555666666666677777777778
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+//*************************   Savings   ****************************************
+//Purpose:  Show rule of 72
+//Inputs:   Inputs to the function here -> Description, Range, Units
+//  p->Initial balance in $'s
+//  i->Decimal value for the interest/year
+//  n->Number of compounding periods, typically by year
+//Output:   Outputs to the function here -> Description, Range, Units
+//  Savings after n compounding periods with i interest rate and p principle
+//******************************************************************************
+
+float save5(float p,float i,int n){
+    float fv=p;
+    for(int year=1;year<=n;year++){
+        int ifv=fv*100;//Modify value for fractional pennies
+        fv=ifv/100.0f; //Modify value for fractional pennies
+        fv*=(1+i);
+    }
+    return fv;
+}
+
 
 //000000011111111112222222222333333333344444444445555555555666666666677777777778
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
