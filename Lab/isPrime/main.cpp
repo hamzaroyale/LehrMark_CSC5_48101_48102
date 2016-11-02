84a0e290d170fe6f11b86deb59268ce5e5fcea54
ml1150258 <mark.lehr@rcc.edu>
2016-11-02 10:51:22 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Lab/isPrime/main.cpp
+++ Lab/isPrime/main.cpp
@@ -3,6 +3,8 @@
    Author: Dr. Mark E. Lehr
    Created on Nov 1st, 2016, 12:00 PM
    Purpose:  Find all Primes
+ *           Utilize with and without the approximation of Square Root
+ *           Utilize with and without the limit on prime search
  */
 
 //System Libraries
@@ -16,14 +18,15 @@ using namespace std;  //Name-space used in the System Library
 //Global Constants
 
 //Function prototypes
-bool isPrime(int);
+bool  isPrime(int);
+float apxSqrt(int);//Approximate Square Root
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare counter
     int count=0,columns=10;
     //Display Output
-    for(int i=-100;i<=1000;i++){
+    for(int i=-100;i<=100000;i++){
         if(isPrime(i)){
             cout<<setw(3)<<i<<" ";
             count++;
@@ -38,9 +41,22 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+float apxSqrt(int n){
+    float guess=n/2,r;
+    float tol=1e-1f;
+    do{
+        r=n/guess;
+        guess=(r+guess)/2;
+    }while((guess-r)>tol);
+    return guess;
+}
+
 bool isPrime(int n){
     if(n<2)return false;
-    for(int i=2;i<=sqrt(n);i++){
+    float sqrtn=apxSqrt(n);//Approximate Square Root -> Babylonians
+    //float sqrtn=sqrt(n);
+    //for(int i=2;i<=n-1;i++){//Inefficient
+    for(int i=2;i<=sqrtn;i++){//Efficient
         if(n%i==0)return false;
     }
     return true;
